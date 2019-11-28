a29bc8a8312188868a54b43dff8f0d8b02a98186
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-28 16:01:24 +0800

    pep8 docstrings

--- vchr_site/vouchers/apps.py
+++ vchr_site/vouchers/apps.py
@@ -1,5 +1,6 @@
+'''Configuration module for vouchers app'''
 from django.apps import AppConfig
 
-
 class VouchersConfig(AppConfig):
+    '''configuration class for vouchers app'''
     name = 'vouchers'
