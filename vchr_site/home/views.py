8c56031baa5e825a2d2f5ca3b8e83fad39defeee
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-28 15:58:56 +0800

    pep8 docstrings

--- vchr_site/home/views.py
+++ vchr_site/home/views.py
@@ -1,5 +1,6 @@
+'''This module contains the function to render the html for home page'''
 from django.shortcuts import render
 
-# Link to template
 def home_navigate(request):
+    '''This function returns a render of the html template'''
     return render(request, 'home/home_navigate.html', {})
