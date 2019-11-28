f3b96f7170c6aa4911aa09400f4801ffc8277b40
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-28 15:57:59 +0800

    pep8 docstrings

--- vchr_site/home/urls.py
+++ vchr_site/home/urls.py
@@ -1,8 +1,8 @@
+'''This module contains the path string for url'''
 from django.urls import path
-
 from . import views
 
 # define urlpatterns
 urlpatterns = [
-    path('', views.home_navigate, name= 'home')
+    path('', views.home_navigate, name='home')
 ]
