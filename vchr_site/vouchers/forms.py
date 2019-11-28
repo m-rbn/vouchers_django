68a539df192834baa1af2af8261488f02bfb6e4b
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-28 16:02:08 +0800

    Update forms.py

--- vchr_site/vouchers/forms.py
+++ vchr_site/vouchers/forms.py
@@ -1,4 +1,7 @@
+'''This module contains the form class to be rendered alongside the html template'''
 from django import forms
 
-class vchr_form(forms.Form):
-    code_input = forms.IntegerField(label = 'CODE')
+class VchrForm(forms.Form):
+    '''The form contains only one character field  data'''
+
+    code_input = forms.CharField(label='CODE', max_length=5)
