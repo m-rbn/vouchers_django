6bab9bb06d3db375d209774e6609da4a0d7f05c2
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-29 03:57:45 +0800

    Update tests.py

--- vchr_site/home/tests.py
+++ vchr_site/home/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+''This module contains tests for home app'''
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(SimpleTestCase):
+    '''
+    Tests:
+        test_home_status_code: Checks for succesful http request.
+        test_view_url_name: Checks for succesful http request when using u>
+        test_view_correct_template: Check if correct template is used.
+
+    '''
+
+    def test_home_status_code(self):
+        '''Checks for succesful http request'''
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_name(self):
+        '''Checks for successful http request when using url name'''
+
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_correct_template(self):
+        '''Check if correct template is used'''
+
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home/home_navigate.html')
