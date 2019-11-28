155b07785c2391edec360853958adbf7a9f0580d
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-28 16:03:14 +0800

    pep8 docstrings

--- vchr_site/vouchers/models.py
+++ vchr_site/vouchers/models.py
@@ -1,40 +1,60 @@
+'''This module contains the  Voucher model used by the vouchers app'''
 from django.db import models
 
 # Voucher code
 class Voucher(models.Model):
-    code_field =  models.IntegerField()                   #   Voucher code to be inputted
-    discount_field = models.FloatField()                  #   How much discount to give
-                                                    #   (if whole number, RM [value] off)
-                                                    #   (if float, [value]% off)
-    usage_no_field = models.IntegerField(default=0)       #   How many times it has been used
-    usage_limit_field = models.IntegerField(default=3)    #   How many time it can be used
+    '''
+    Voucher is a model with four fields containing attributes of a voucher code.
 
-    # def __str__
+    Fields:
+        code_field: Voucher code to be inputted
+        discount_field: How much discount to give.
+                        If whole number, RM (value) off.
+                        If float, (value)% off.
+        usage_no_field: How many times the voucher code has been used.
+        usage_limit_field: How many time the voucher code can be used.
+
+    Methods:
+        self.validate: validate method to check validity of voucher code.
+                       Returns the validity status of the code.
+        self.discount_rate: discount_rate method to return discount value string.
+        self.accept: accept method to accept value voucher code and modify the usage_no_field.
+
+    '''
+    objects = models.Manager()
+    code_field = models.CharField(max_length=5)
+    discount_field = models.FloatField()
+    usage_no_field = models.IntegerField(default=0)
+    usage_limit_field = models.IntegerField(default=3)
+
+
     def __str__(self):
-        return str(self.code_field)
+        code_str = str(self.code_field)
+        return code_str
 
-    # def validate method to check validity
     def validate(self):
+        '''Validate method to check validity of boucher code'''
         if self.usage_no_field >= self.usage_limit_field:
             return 'Invalid'
-        else:
-            return 'Valid'
+        return 'Valid'
 
-    # def method to retun discount string
     def discount_rate(self):
-        if self.validate() ==  'Valid':
-            if self.discount_field.is_integer() == True:
-                return 'RM'+str(int(self.discount_field))+' off'
-            else:
-                return str(int(self.discount_field*100))+'% off'
-        else:
-            return 'N/A'
+        '''Method to return discount value string'''
+        if self.validate() == 'Valid':
+            if float(self.discount_field).is_integer() is True:
+                rm_str = 'RM'+str(int(self.discount_field))+' off'
+                return rm_str
+            pct_str = str(int(self.discount_field*100))+'% off'
+            return pct_str
+        return 'N/A'
 
-    # def method to show whether voucher accepted or not
     def accept(self):
-        if self.validate() ==  'Valid':
+        '''Method to show whether voucher is accepted or not'''
+        if self.validate() == 'Valid':
             self.usage_no_field += 1
             self.save()
-            return '* Voucher accepted. Can be reused '+ str(self.usage_limit_field-self.usage_no_field) + ' more times'
-        else:
-            return '* Voucher not accepted. Exceeded voucher usage limit.'
+            n_left = str(self.usage_limit_field-self.usage_no_field)
+            accept_str = '* Voucher accepted. Can be reused '+ n_left + ' more times.'
+            return accept_str
+        not_accept_str = '* Voucher not accepted. Exceeded voucher usage limits.'
+        return not_accept_str
