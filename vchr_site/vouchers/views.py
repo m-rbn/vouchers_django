fc2fe5b31b1706eefae45dce5ca65a060b6a4ef6
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-28 16:04:22 +0800

    Update views.py

--- vchr_site/vouchers/views.py
+++ vchr_site/vouchers/views.py
@@ -1,34 +1,41 @@
+'''Module to render the html template with form for entering voucher code'''
 from django.shortcuts import render
-from vouchers.forms import vchr_form
 from django.template import loader
 from django.http import HttpResponse
+from vouchers.forms import VchrForm
 from vouchers.models import Voucher
 
 # render the code form
-def RenderForm(request):
+def render_form(request):
+    '''
+    This function renders the form inside the html template.
+    When a GET request is sent, it will render the voucher status page if the voucher code is valid
+
+    '''
+
     # init form
-    form =  vchr_form()
+    form = VchrForm()
 
     # For when form is submitted
-    submit_Form = vchr_form(request.GET)
+    submit_form = VchrForm(request.GET)
 
     # If the form is valid, return the voucher status page
-    if submit_Form.is_valid():
+    if submit_form.is_valid():
 
         # Retrieve code input from form
-        vchr_code = submit_Form.cleaned_data['code_input']
+        vchr_code = submit_form.cleaned_data['code_input']
 
         # Check if code exist in model, if exist: return voucher status page,
         # if not: voucher not found page
-        if Voucher.objects.filter(code_field = vchr_code).exists():
+        if Voucher.objects.filter(code_field=vchr_code).exists():
 
             # create dictionary for voucher information to be displayed in
             # voucher status page
             context = {
-            'code': vchr_code,
-            'status': Voucher.objects.get(code_field = vchr_code).validate(),
-            'discount': Voucher.objects.get(code_field = vchr_code).discount_rate(),
-            'acceptance': Voucher.objects.get(code_field = vchr_code).accept()
+                'code': vchr_code,
+                'status': Voucher.objects.get(code_field=vchr_code).validate(),
+                'discount': Voucher.objects.get(code_field=vchr_code).discount_rate(),
+                'acceptance': Voucher.objects.get(code_field=vchr_code).accept()
             }
 
             # load html for voucher status page
@@ -36,11 +43,9 @@ def RenderForm(request):
 
             return HttpResponse(template_stat.render(context, request))
 
-        else:
+        # load html for voucher not found
+        template_not_found = loader.get_template('vouchers/voucher_not_found.html')
 
-            # load html for voucher not found
-            template_not_found = loader.get_template('vouchers/voucher_not_found.html')
-
-            return HttpResponse(template_not_found.render())
+        return HttpResponse(template_not_found.render())
 
     return render(request, 'vouchers/vouchers.html', {'form':form})
