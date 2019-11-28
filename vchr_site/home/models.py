eda04f762dcc6575e224c8fe66e500841e876555
m-rbn <39186921+m-rbn@users.noreply.github.com>
2019-11-28 15:57:04 +0800

    pep8 docstrings

--- vchr_site/home/models.py
+++ vchr_site/home/models.py
@@ -1,4 +1,4 @@
-'''No model umis used currently so is commented out for future use'''
+'''No model is used currently so is commented out for future use'''
 #from django.db import models
 
 # Create your models here.
